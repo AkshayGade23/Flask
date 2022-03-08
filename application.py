29567cba3520491b49f4647f5e61d0dc6e6ccc4a
Vijay Jha <jvdvijay@gmail.com>
2022-03-08 16:21:46 +0530

    Feeling Bug fixed

--- application.py
+++ application.py
@@ -3,18 +3,20 @@ from flask import Flask, jsonify, request
 import nltk
 import text2emotion as text
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/feeling')
+@application.route('/feeling')
 def feelingController():
     response = {}
     response['query'] = str(request.args['query'])
     response['output'] = text.get_emotion(response['query'])
     response['emotion'] = max(response['output'], key= lambda x: response['output'][x])
+    if response['emotion'] == 'Surprise':
+        response['emotion'] = 'Happy'
     return jsonify(response)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    application.run()
 
 nltk.download('omw-1.4')    # To make the nltk up-to-date
